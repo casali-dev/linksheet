117db5a3195aad910a5d235ae07f580d2aedeb22
Guilherme Casali <guilhermecasali.dev@gmail.com>
2025-06-18 12:09:17 -0300

    feat: implement graceful shutdown with context and signal handling

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
-	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/casali-dev/linksheet/db"
 	"github.com/casali-dev/linksheet/router"
@@ -13,18 +16,37 @@ import (
 func main() {
 	db.Connect()
 	defer db.Close()
-
 	db.RunMigrations()
 
-	ln, err := net.Listen("tcp", ":3333")
-	if err != nil {
-		log.Fatal("Erro ao iniciar o servidor:", err)
+	server := &http.Server{
+		Addr:    ":3333",
+		Handler: router.Handler(),
 	}
 
-	log.Println("Servidor rodando na porta 3333")
-	err = http.Serve(ln, router.Handler())
-	if err != nil {
-		log.Printf("Erro no servidor: %v", err)
-		os.Exit(1)
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Println("Servidor rodando na porta 3333")
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("Erro no servidor: %v", err)
+
+	case sig := <-sigint:
+		log.Printf("Sinal capturado: %v. Encerrando com graceful shutdown...", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Erro ao finalizar servidor: %v", err)
+		} else {
+			log.Println("Servidor encerrado com sucesso.")
+		}
 	}
 }
