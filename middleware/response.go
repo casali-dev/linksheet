0729ee1c4781caad9a2e1e7bca0bcd7d2e4ded9d
Guilherme Casali <guilhermecasali.dev@gmail.com>
2025-06-18 12:46:46 -0300

    feat: add standardized JSON response helpers

--- middleware/response.go
+++ middleware/response.go
@@ -1,6 +1,36 @@
 package middleware
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type response struct {
+	Message string      `json:"message,omitempty"`
+	Error   string      `json:"error,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func WriteError(w http.ResponseWriter, status int, msg string) {
+	w.WriteHeader(status)
+
+	res := response{
+		Error: msg,
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
+
+func WriteJSON(w http.ResponseWriter, status int, data interface{}, msg string) {
+	w.WriteHeader(status)
+
+	res := response{
+		Message: msg,
+		Data:    data,
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
 
 func JSONMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
